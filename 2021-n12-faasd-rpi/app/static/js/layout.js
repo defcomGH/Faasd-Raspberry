520659494adecab7db16a1d01a7e720db1fb3da0
inakizabalza33 <inaki.zabalzadiaz@gmail.com>
2022-10-24 23:30:26 +0100

    supuestamente anda

---
+++ 2021-n12-faasd-rpi/app/static/js/layout.js
@@ -0,0 +1,89 @@
+
+function actualizarChart(valores,chartdiv){
+    diccionario= {
+        "type": "serial",
+        "categoryField": "category",
+        "startDuration": 0,
+        "addClassNames": true,
+        "backgroundColor": "#F5F3F5",
+        "backgroundAlpha": 1,
+        "categoryAxis": {
+            "axisThickness": 2,
+            "color": "#333333",
+            "dashLength": 5,
+            "gridAlpha": 2,
+            "tickLength": 2
+        },
+        "chartCursor": {
+            "enabled": true,
+            "bulletsEnabled": true,
+            "bulletSize": 20,
+            "categoryBalloonColor": "blue"/*"#FCD6A9"*/,
+            "color": "#000000",
+            "cursorColor": "blue"/*"#FCD6A9"*/,
+            "graphBulletAlpha": 0,
+            "graphBulletSize": 0,
+            "selectionAlpha": 0,
+            "selectWithoutZooming": true,
+            "valueLineAlpha": 0,
+            "zoomable": false
+        },
+        "trendLines": [],
+        "graphs": [
+            {
+                "balloonColor": "#0331c991"/*"#FCD6A9"*/,
+                "bullet": "round",
+                "bulletBorderAlpha": 1,
+                "bulletBorderColor": "#2684FC",
+                "bulletColor": "#FFFFFF",
+                "color": " #2684FC",
+                "cursorBulletAlpha": 0,
+                "customBullet": "",
+                "fillAlphas": 0.5,
+                "fillToAxis": "x",
+                "fontSize": 20,
+                "id": "AmGraph-2",
+                "title": chartdiv,
+                "labelPosition": "bottom",
+                "lineColor": " #2684FC",
+                "lineThickness": 3,
+                "type": "smoothedLine",
+                "valueField": "column-1"
+            }
+        ],
+        "guides": [],
+        "valueAxes": [
+            {
+                "id": "ValueAxis-1",
+                "maximum": 40,
+                "minimum": 20,
+                "axisThickness": 0,
+                "color": "#333333",
+                "dashLength": 5,
+                "gridAlpha": 2,
+                "labelFrequency": 2,
+                "tickLength": 0,
+                "title": ""
+            }
+        ],
+        "allLabels": [],
+        "balloon": {},
+        "legend": {
+            "enabled": false,
+            "align": "right",
+            "position": "top",
+            "useGraphSettings": true
+        },
+        "titles": [],
+        "dataProvider": []
+    }
+    for (let i in valores.reverse()) {
+        minidiccionario= {
+            "category": valores[i].timestamp,
+            "column-1": valores[i].valor
+        }
+        diccionario.dataProvider.push(minidiccionario)
+    }
+
+    AmCharts.makeChart(chartdiv,diccionario);
+}
---
+++ 2021-n12-faasd-rpi/raspberry/scripts/scriptdos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#cd
+## Descarga de los binarios de buildkit
+#wget -qO- https://github.com/moby/buildkit/releases/download/v0.9.2/buildkit-v0.9.2.linux-arm-v7.tar.gz | tar -xz -C /usr/local/bin/ --strip-components=1
+## Autenticacion para pushear a un registro de docker
+#mkdir -p ~/.docker && cd ~/.docker
+#touch config.json
+#export DOCKER_AUTH=$(echo -n "$DOCKERHUB_USERNAME:$DOCKERHUB_TOKEN" | base64)
+#cat > ~/.docker/config.json << EOF
+#{
+#    "auths": {
+#        "https://index.docker.io/v1/": {
+#            "auth": "$DOCKER_AUTH"
+#        }
+#    }
+#}
+#EOF
+## Inicio del buildkit daemon, pusheo de la funcion a dockerhub
+/usr/local/bin/buildkitd &
