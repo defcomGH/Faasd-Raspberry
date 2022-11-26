8a0e8f71fe854288e51d91810772fd660d2c0f3d
inakizabalza33 <inaki.zabalzadiaz@gmail.com>
2022-11-26 03:21:10 +0000

    cambio en el 15 para que tengan el mismo endpoint que el 20

--- 2021-n12-faasd-rpi/openfaas/localhost/buildFunction.sh
+++ 2021-n12-faasd-rpi/openfaas/localhost/buildFunction.sh
@@ -1,9 +1,9 @@
 #faas build -f functemp.yml --shrinkwrap
-varDirectory=temperatura
+varDirectory=humedadlocal
 sudo buildctl build \
     --frontend dockerfile.v0 \
     --local context=build/$varDirectory \
     --local dockerfile=build/$varDirectory/ \
-    --output type=image,name=docker.io/evaristo00/$varDirectory"15":latest,push=true
+    --output type=image,name=docker.io/evaristo00/humedadlocal20:latest,push=true
     
 #faas deploy -f prueba.yml
