614c9c282f198b75358bddbdb3de2f7fadf48f01
inakizabalza33 <inaki.zabalzadiaz@gmail.com>
2022-12-01 21:38:30 +0000

    agregado de archivo local con funciones de ip openfaas para pegarle al localhost

--- 2021-n12-faasd-rpi/openfaas/local/temperatura/handler.py
+++ 2021-n12-faasd-rpi/openfaas/local/temperatura/handler.py
@@ -2,7 +2,7 @@ def handle(req): #Funcion que se comunica con el servidor y devuelve la humedad
 
     import requests
 
-    url= 'http://10.62.0.1:6000/humedad'
+    url= 'http://10.62.0.1:6000/temperatura'
 
     humedad= requests.get(url).text
 
